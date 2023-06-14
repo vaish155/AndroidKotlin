073632a093a556a741576111614596b90cee2b6a
vaish155 <svaishnavg@gmail.com>
2023-06-14 14:25:46 +0530

    Implemented View Model

--- mvvmRecyclerApi/app/src/main/java/com/vaishnav/mvvmrecyclerapi/RecyclerView.kt
+++ mvvmRecyclerApi/app/src/main/java/com/vaishnav/mvvmrecyclerapi/RecyclerView.kt
@@ -1,13 +1,19 @@
 package com.vaishnav.mvvmrecyclerapi
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.vaishnav.mvvmrecyclerapi.network.RetroInstance
 import com.vaishnav.mvvmrecyclerapi.network.RetroService
+import com.vaishnav.mvvmrecyclerapi.viewmodels.RecyclerActivityViewModel
 import retrofit2.Call
 import retrofit2.Response
 
@@ -17,11 +23,18 @@ class RecyclerView : AppCompatActivity() {
 
     private lateinit var recyclerViewAdapter : RecyclerViewAdapter
 
+    private lateinit var searchboxid : EditText
+    private lateinit var serachButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
         recyclerView = findViewById(R.id.recyclerView)
+
+        searchboxid = findViewById(R.id.searchboxid)
+        serachButton = findViewById(R.id.serachButton)
+
 
         initRecyclerView()
         createData()
@@ -52,21 +65,41 @@ class RecyclerView : AppCompatActivity() {
 //        recyclerViewAdapter.setListData(item)
 //        recyclerViewAdapter.notifyDataSetChanged()
 
-        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
-        val call = retroInstance.getDatafromAPI("atlanta")
-        call.enqueue(object : retrofit2.Callback<RecyclerList>{
-            override fun onResponse(call: Call<RecyclerList>, response: Response<RecyclerList>) {
-                if(response.isSuccessful)
-                {
-                    recyclerViewAdapter.setListData(response.body()?.items!!)
-                    recyclerViewAdapter.notifyDataSetChanged()
-                }
-            }
+//        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+//        val call = retroInstance.getDatafromAPI("atlanta")
+//        call.enqueue(object : retrofit2.Callback<RecyclerList>{
+//            override fun onResponse(call: Call<RecyclerList>, response: Response<RecyclerList>) {
+//                if(response.isSuccessful)
+//                {
+//                    recyclerViewAdapter.setListData(response.body()?.items!!)
+//                    recyclerViewAdapter.notifyDataSetChanged()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<RecyclerList>, t: Throwable) {
+//                Toast.makeText(this@RecyclerView, "Error in getting data from API", Toast.LENGTH_SHORT).show()
+//            }
+//        })
 
-            override fun onFailure(call: Call<RecyclerList>, t: Throwable) {
+
+
+        val viewModel = ViewModelProviders.of(this).get(RecyclerActivityViewModel::class.java)
+        viewModel.getRecyclerListDataObserver().observe(this, Observer<RecyclerList>{
+            if(it != null)
+            {
+                recyclerViewAdapter.setListData(it.items)
+                recyclerViewAdapter.notifyDataSetChanged()
+            }
+            else
+            {
                 Toast.makeText(this@RecyclerView, "Error in getting data from API", Toast.LENGTH_SHORT).show()
             }
         })
+
+        serachButton.setOnClickListener {
+            viewModel.makeApiCall(searchboxid.text.toString())
+        }
+
     }
 
 }
